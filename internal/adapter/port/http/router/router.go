f484b56b708a6bbe9fb2b55f564afdefb70a07c5
trevor atkinson <tsa@structx.io>
2024-04-20 16:57:29 -0600

    remove unnecessary files

--- internal/adapter/port/http/router/router.go
+++ internal/adapter/port/http/router/router.go
@@ -33,13 +33,13 @@ func New(logger *zap.Logger) chi.Router {
 	for _, c := range cc {
 
 		if r0, ok := c.(controller.V0); ok {
-			handler := r0.RegisterRoutesV0()
-			r.Mount("/", handler)
+			h := r0.RegisterRoutesV0()
+			r.Mount("/", h)
 		}
 
 		if r1, ok := c.(controller.V1); ok {
-			handler := r1.RegisterRoutesV1()
-			v1.Mount("/", handler)
+			h := r1.RegisterRoutesV1()
+			v1.Mount("/", h)
 		}
 	}
 
